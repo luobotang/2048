4dd2ae0d0c5f8ec95765b9131645e8023f277d54
luobotang <tangkangxing@sina.com>
2014-06-13 16:40:51 +0800

    调整项目文件结构
    js，css，less
    源码：js/src/*.js, less/2048.less
    发布：js/src/2048.min.js, css/2048.css

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,12 +4,12 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     watch: {
       css: {
-        files: ["*.less"],
+        files: ["less/*.less"],
         tasks: ['less']
       },
       js: {
         files: [
-          'assets/js/*.js',
+          'js/src/*.js',
           'Gruntfile.js'
         ],
         tasks: ['jshint']
@@ -19,7 +19,7 @@ module.exports = function(grunt) {
       options: {
         jshintrc: '.jshintrc'
       },
-      all: ['Gruntfile.js', '2048.js']
+      all: ['Gruntfile.js', 'js/src/2048_game.js', 'js/src/2048_numbers.js']
     },
     less: {
       development: {
@@ -27,7 +27,24 @@ module.exports = function(grunt) {
           compress: true
         },
         files: {
-          "2048.css": "2048.less"
+          "css/2048.css": "less/2048.less"
+        }
+      }
+    },
+    concat: {
+      dist: {
+        src: ['js/src/2048_game.js', 'js/src/2048_numbers.js'],
+        dest: 'js/dist/2048.js'
+      }
+    },
+    uglify: {
+      options: {
+        compress: true,
+        banner: "/* 2048 by luobotang\n   tangkangxing@sina.com\n*/\n"
+      },
+      compress: {
+        files: {
+          "js/dist/2048.min.js": "js/dist/2048.js"
         }
       }
     }
@@ -35,7 +52,9 @@ module.exports = function(grunt) {
   // 加载Grunt插件
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   // 注册grunt默认任务
   grunt.registerTask('default', ['watch']);
 };
