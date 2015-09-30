2c1b6686326039734d82dda6e140cd0fd788cc2d
luobotang <tangkangxing@sina.com>
2015-09-30 11:33:13 +0800

    refactor

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function(grunt) {
-  // 项目配置
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     watch: {
@@ -8,53 +8,28 @@ module.exports = function(grunt) {
         tasks: ['less']
       },
       js: {
-        files: [
-          'js/src/*.js',
-          'Gruntfile.js'
-        ],
-        tasks: ['jshint']
+        files: ['js/src/*.js'],
+        tasks: ['uglify']
       }
-    },
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      all: ['Gruntfile.js', 'js/src/2048_game.js', 'js/src/2048_numbers.js']
     },
     less: {
-      development: {
-        options: {
-          compress: true
-        },
-        files: {
-          "css/2048.css": "less/2048.less"
-        }
-      }
-    },
-    concat: {
-      dist: {
-        src: ['js/src/2048_game.js', 'js/src/2048_numbers.js'],
-        dest: 'js/dist/2048.js'
+      build: {
+        src: 'less/2048.less',
+        dest: '2048.css'
       }
     },
     uglify: {
-      options: {
-        compress: true,
-        banner: "/* 2048 by luobotang\n   tangkangxing@sina.com\n*/\n"
-      },
-      compress: {
-        files: {
-          "js/dist/2048.min.js": "js/dist/2048.js"
-        }
+      build: {
+        src: ['lib/game.js', 'lib/numbers.js'],
+        dest: "2048.min.js"
       }
     }
-  });
-  // 加载Grunt插件
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  // 注册grunt默认任务
-  grunt.registerTask('default', ['watch']);
-};
+  })
+
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-contrib-less')
+  grunt.loadNpmTasks('grunt-contrib-uglify')
+
+  grunt.registerTask('default', ['less', 'uglify'])
+  grunt.registerTask('watch', ['watch'])
+}
