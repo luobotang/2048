2e9fd7319d15a05023e93c80b4cc30869e135129
luobotang <tangkagnxing@sina.com>
2016-12-30 13:44:04 +0800

    support online cache

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,14 +1,22 @@
+var cacheFiles = [
+  '/',
+  '/index.html',
+  '/jquery-2.1.1.min.js',
+  '/app.css',
+  '/app.js',
+  '/404.html'
+];
+
+if (location.protocal === 'https') {
+  cacheFiles = cacheFiles.map(function (path) {
+    return '/2048/app' + path;
+  });
+}
+
 this.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/jquery-2.1.1.min.js',
-        '/app.css',
-        '/app.js',
-        '/404.html'
-      ]);
+      return cache.addAll(cacheFiles);
     })
   );
 });
